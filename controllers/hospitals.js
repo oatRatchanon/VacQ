18d015a3d5bd1b469998e1e17174c7bfcaa1142a
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2023-03-23 13:04:10 +0700

    add VacCenter API

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,4 +1,20 @@
 const Hospital = require("../models/Hospital");
+const vacCenter = require("../models/VacCenter");
+
+//@desc     Get vaccine centers
+//@route    GET /api/v1/hospitals/vacCenters/
+//@access   Public
+exports.getVacCenters = (req, res, next) => {
+  vacCenter.getALL((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while retrieving Vaccine Centers.",
+      });
+    else res.send(data);
+  });
+};
+
 //@desc     Get all hospitals
 //@route    GET /api/v1/hospitals
 //@access   Public
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -5,6 +5,7 @@ const {
   createHospital,
   updateHospital,
   deleteHospital,
+  getVacCenters,
 } = require("../controllers/hospitals");
 const { protect, authorize } = require("../middleware/auth");
 
@@ -15,6 +16,8 @@ const router = express.Router();
 
 //Re-route into other resource routers
 router.use("/:hospitalId/appointments/", appointmentRouter);
+
+router.route("/vacCenters").get(getVacCenters);
 
 router
   .route("/")
